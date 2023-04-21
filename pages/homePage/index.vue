ba19ab2f70396ae7f8def6b4e500894fba8d3698
Aljaz Razdevsek <razdevsekaljaz1994@gmail.com>
2023-04-21 18:43:04 +0200

    Dodan api za login userja Dodane podlage za page Track, Home

---
+++ pages/homePage/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container"></div>
+</template>
+
+
+<script>
+import clock from "~/components/utils/clock.vue";
+export default {
+  name: "HomePage",
+  components: { clock },
+  async created() {
+    this.user = await this.$api.users.getUser(1);
+  },
+  data: function () {
+    return {
+      user: null,
+      username: "",
+      password: "",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
---
+++ pages/homePage/track/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+import clock from "~/components/utils/clock.vue";
+export default {
+  name: "Track",
+  components: { clock },
+  async created() {},
+  data: function () {
+    return {
+      user: null,
+      username: "",
+      password: "",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
